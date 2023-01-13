4b2dfbf9b73774482f2d220ef8e24017e9f7fc1f
Fineslow <jemiranda003@gmail.com>
2023-01-13 15:59:34 -0500

    fixing first erros

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Homepage from './views/homePage';
+import HomePage from './views/homePage/HomePage';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
