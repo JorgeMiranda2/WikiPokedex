795312171b970463da289d77e66820b8b6a37c91
Fineslow <jemiranda003@gmail.com>
2023-01-13 15:56:54 -0500

    creating folder organization and basic modules

---
+++ src/views/homePage/HomePage.jsx
@@ -0,0 +1,17 @@
+import NovelCard from "../../components/common/NovelCard.jsx";
+import Axios from "axios";
+import { useEffect,useState } from "react";
+
+
+
+
+
+
+const HomePage = () => {
+
+<div>
+  
+</div>  
+
+}
+export default HomePage;
