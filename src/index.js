c52944255d48b0fb8b02c714948861bef9395df6
Fineslow <jemiranda003@gmail.com>
2023-01-15 16:01:42 -0500

    inicio

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import HomePage from './views/homePage/HomePage';
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
