048f655e756cb2b95798b3c88342e9562cd6a05d
Fineslow <jemiranda003@gmail.com>
2023-01-15 20:17:02 -0500

    agregado navbar secundario

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,12 +1,47 @@
-const NavBar = () => {
+import "../../Styles/ComponentsCss/NavBar2.scss";
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+const NavBar2 = () => {
+
+        const [showNavbar, setShowNavbar] = useState(false)
+
+    const handleShowNavbar = () => {
+        setShowNavbar(!showNavbar)
+      }
+    
     return ( 
         <div>
-            <h2>Navbar</h2>
+             <nav className="navbar">
+      <div className="container">
+        <div className="logo">
+          <h2>awa</h2>
+        </div>
+        <div className="menu-icon" onClick={handleShowNavbar}>
+        <h2>owo</h2>
+        </div>
+        <div className={`nav-elements  ${showNavbar && 'active'}`}>
+          <ul>
+            <li>
+              <a to="/">Home</a>
+            </li>
+            <li>
+              <a to="/blog">Blog</a>
+            </li>
+            <li>
+              <a to="/projects">Projects</a>
+            </li>
+            <li>
+              <a to="/about">About</a>
+            </li>
+            <li>
+              <a to="/contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
         </div>
      );
 }
  
-export default NavBar;
-<div>
-    <h2>Navbar</h2>
-</div>
+export default NavBar2;
--- src/views/homePage/HomePage.js
+++ src/views/homePage/HomePage.js
@@ -1,12 +1,14 @@
 import PokeCardContainer from "../../components/common/PokeCardContainer";
 import NavBar from "../../components/layout/NavBar" 
 import PokeSearch from "../../components/common/PokeSearch";
+import completeInfo from "../../components/hooks/PokeInfo";
 const HomePage = () => {
   return ( 
     <div>
       <NavBar/>
       <PokeSearch/>
       <PokeCardContainer/>
+     
 
     </div>
    );
