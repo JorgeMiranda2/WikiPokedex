5d2761dc8a89b8db6bde6a07c8c9ff5ee8d5e117
Zenway <zenway15@gmail.com>
2023-01-16 22:06:16 -0500

    Card

--- src/components/common/PokeCard.js
+++ src/components/common/PokeCard.js
@@ -1,9 +1,19 @@
+import '../../../src/Styles/ComponentsCss/PokeCard.scss';
+import pokemon from '../../assets/pokemon.png';
 
 const PokeCard = () => {
     return ( 
-        <div>
-         <h2>card</h2>   
-        </div>
+        <div class="carousel-item">
+                <img class="carousel-item__img" src={ pokemon } alt="Pokemon"></img>
+                <div class="carousel-item__details">
+                    <div>
+                        <img src="../public/icons/icon-play.png" alt="Play Icon"></img>
+                        <img src="../public/icons/icon-plus.png" alt="Plus Icon"></img>
+                    </div>
+                    <p class="carousel-item__details--title">POKEMON</p>
+                    <p class="carousel-item__details--subtitle">Ta buenardo</p>
+                </div>
+            </div>
      );
 }
  
--- src/components/common/PokeCardContainer.js
+++ src/components/common/PokeCardContainer.js
@@ -1,12 +1,16 @@
+import '../../../src/Styles/ComponentsCss/PokeCard.scss';
 import PokeCard from "./PokeCard";
 import {GetPokeInfo} from "../hooks/PokeInfo";
 const PokeCardContainers = () => {
     const completeInfo = GetPokeInfo();
     return ( 
-        <div>
+        <section class="carousel">
+        <div class="carousel__container">
             <h2>{completeInfo}</h2>
             <PokeCard/>
         </div>
+        </section>
+        
      );
 }
  
