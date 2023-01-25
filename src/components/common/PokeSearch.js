31448065704a9e2d8b3012e4d8677cf9e85a8f53
Zenway <zenway15@gmail.com>
2023-01-24 21:09:14 -0500

    Searchbar 1/2

--- src/components/common/PokeSearch.js
+++ src/components/common/PokeSearch.js
@@ -1,15 +1,47 @@
 import '../../Styles/ComponentsCss/PokeSearch.scss';
-const PokeSearch = () => {
-    return ( 
-        <section class="main">
-        <h2 class="main__title">¿Qué Pokemon buscas?</h2>
+import axios from 'axios';
+import React from "react";
+import { searchPokemon } from '../../api';
+const { useState } = React;
+
+const PokeSearch = (props) => {
+  const { onSearch } = props;
+  const [search, setSearch] = useState('');
+
+
+  const onChange = (e) => {
+    setSearch(e.target.value);
+    if (e.target.value.length === 0) {
+      onSearch(null);
+    }
+  };
+
+  const onClick = async (e) => {
+    const data = await searchPokemon(search);
+    console.log(data);
+  };
+
+
+
+
+  return ( 
+      <div>
+        <section className="main">
+        <h2 className="main__title">¿Qué Pokemon buscas?</h2>
+        <div className="justify-content-center input-group">
         <input
-          class="input"
-          type="text"
+          className="inputp"
+          onChange={onChange}
           placeholder="Buscar... (Escribe el nombre del Pokemon)"
         />
+        <button className="button-search" onClick={onClick}>
+          <i className="bi-search"></i>
+        </button>
+        </div>
+        
       </section>
+      </div>
+      
         );
 }
- 
 export default PokeSearch;
